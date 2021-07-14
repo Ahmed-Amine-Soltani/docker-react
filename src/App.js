9ba8f0ff20d52170d34921c2aab6fd1de4cba0a3
Ahmed-Amine-Soltani <soltani.ahmedamine1@gmail.com>
2021-07-14 22:56:11 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there  !!
+          Yassine lapip
         </p>
         <a
           className="App-link"
